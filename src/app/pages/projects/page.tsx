565d732df167873f8e1001635cd8675f2ff4ce6e
Mica <micaolor@gmail.com>
2025-06-08 20:37:34 -0300

    making first comps

--- src/app/pages/projects/page.tsx
+++ src/app/pages/projects/page.tsx
@@ -1,12 +1,8 @@
-export default function Home() {
+export default function project() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col row-start-2 items-center sm:items-start">
-        Micaela Solórzano
-        <p>Frontend developer</p>
-        <a href="/pages/about">
-          <button className="cursor-pointer p-2 bg-amber-300">Click me</button>
-        </a>
+        Helloo :D
       </main>
     </div>
   );
